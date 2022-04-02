8b2e99a1af8b4b00200568cbf84da079ffcd0589
iamjiri <thisisjiri@gmail.com>
2022-04-02 20:55:03 +0200

    updated function for showing description of projects. now the function works for every project

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 let project1 = document.getElementById('project1');
 let project1Text = document.getElementById('project1_text');
 
-function showText() {
-    project1Text.style.display = 'block';
+let project2 = document.getElementById('project2');
+let project2Text = document.getElementById('project2_text');
+
+let project3 = document.getElementById('project3');
+let project3Text = document.getElementById('project3_text');
+
+
+function showText(event) {
+    switch (event.target) {
+        case project1:
+            project1Text.style.display = 'block';
+            break;
+        case project2:
+            project2Text.style.display = 'block';
+            break;
+        case project3:
+            project3Text.style.display = 'block';
+            break;
+        default:
+            alert('invalid');
+    };
 };
 
 project1.addEventListener('click', showText);
+project2.addEventListener('click', showText);
+project3.addEventListener('click', showText);
