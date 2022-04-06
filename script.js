bd81b23cd3283ba52674039c24ccc2044b14ec8b
iamjiri <thisisjiri@gmail.com>
2022-04-06 18:06:34 +0200

    functionality of JS function that shows or hides info about projects is completed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let project1 = document.getElementById('project1');
 let project1Text = document.getElementById('project1_text');
+let project1Hide = document.getElementById('project1Hide');
 
 let project2 = document.getElementById('project2');
 let project2Text = document.getElementById('project2_text');
@@ -12,21 +13,54 @@ function showText(event) {
     switch (event.target) {
         case project1:
             project1Text.style.display = 'block';
-            project1.innerHTML = 'Show less';
+            project1.style.display = 'none';
+            project1Hide.style.display = 'block';
+            project1Hide.style.cursor = 'pointer';
             break;
         case project2:
             project2Text.style.display = 'block';
-            project2.innerHTML = 'Show less';
+            project2.style.display = 'none';
+            project2Hide.style.display = 'block';
+            project2Hide.style.cursor = 'pointer';
             break;
         case project3:
             project3Text.style.display = 'block';
-            project3.innerHTML = 'Show less';
+            project3.style.display = 'none';
+            project3Hide.style.display = 'block';
+            project3Hide.style.cursor = 'pointer';
             break;
         default:
             alert('invalid');
     };
 };
 
+function hideText(event) {
+    switch (event.target) {
+        case project1Hide:
+            project1Text.style.display = '';
+            project1.style.display = '';
+            project1Hide.style.display = '';
+            break;
+        case project2Hide:
+            project2Text.style.display = '';
+            project2.style.display = '';
+            project2Hide.style.display = '';
+            break;
+        case project3Hide:
+            project3Text.style.display = '';
+            project3.style.display = '';
+            project3Hide.style.display = '';
+            break;
+        default:
+            alert('invalid');
+    }
+}
+
 project1.addEventListener('click', showText);
+project1Hide.addEventListener('click', hideText);
+
 project2.addEventListener('click', showText);
+project2Hide.addEventListener('click', hideText);
+
 project3.addEventListener('click', showText);
+project3Hide.addEventListener('click', hideText);
